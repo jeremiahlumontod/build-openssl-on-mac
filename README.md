233b1deebfdb263e2366cb0faca72767ab04cf96
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-03-31 13:30:08 +0400

    added script and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # build-openssl-on-mac
+
+1) update OPENSSL_VERSION with desired version
+2) update libcrypto and libssl versions if needed. when script is executed it will download this two, and if the version has changed then you need to adjust the script
---
+++ build_openssl_dylib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OPENSSL_VERSION="1.1.0e"
+
+curl -O -L http://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
+tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
+mv openssl-$OPENSSL_VERSION openssl_i386
+tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
+mv openssl-$OPENSSL_VERSION openssl_x86_64
+cd openssl_i386
+./Configure darwin-i386-cc -shared
+make
+cd ../
+cd openssl_x86_64
+./Configure darwin64-x86_64-cc -shared
+make
+cd ../
+lipo -create openssl_i386/libcrypto.1.1.dylib openssl_x86_64/libcrypto.1.1.dylib -output libcrypto.1.1.dylib
+lipo -create openssl_i386/libssl.1.1.dylib openssl_x86_64/libssl.1.1.dylib -output libssl.1.1.dylib
+rm openssl-$OPENSSL_VERSION.tar.gz
